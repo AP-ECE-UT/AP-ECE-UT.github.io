fd03196f89466efcdade2953107d1aa342e6b7e5
rfan sgari <erfanasgari21@gmail.com>
2024-09-05 15:02:28 +0330

    projects pdf test

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,11 +5,21 @@ import Layout from "@theme/Layout";
 import ProjectCard from "../components/ProjectCard";
 import Footer from "@site/src/components/FooterDuck";
 import { useParams } from "react-router-dom";
+import { Document } from "react-pdf";
 
 import Heading from "@theme/Heading";
 import styles from "./ta.module.css";
 import projectsData from "../data/projectsData";
 import { useLocation } from "react-router-dom";
+import { pdfjs } from "react-pdf";
+
+import { Worker } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
+
+// Import the styles
+import "@react-pdf-viewer/core/lib/styles/index.css";
+
+// Your render function
 
 function HomepageHeader() {
     const { siteConfig } = useDocusaurusContext();
@@ -30,15 +40,26 @@ export default function Home() {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const projectId = queryParams.get("project");
+    const mode = queryParams.get("mode");
     let project;
     if (projectId) {
         project = projectsData[0].list.find((pr) => pr.projectId == projectId);
     }
     return project ? (
         <Layout title={project.title} description={project.description}>
-            <Heading as="h1" className="margin-bottom--lg">
-                {project.title}
-            </Heading>
+            {mode == "iframe" ? (
+                <iframe src={`https://docs.google.com/viewer?url=${project.pdf}`} width="100%" height="600px">
+                    This browser does not support iframes.
+                </iframe>
+            ) : mode == "embed" ? (
+                <embed src={project.pdf} width="100%" height="600px" type="application/pdf"></embed>
+            ) : (
+                <object data={project.pdf} type="application/pdf" width="100%" height="100%">
+                    <p>
+                        <a href={project.pdf}>لینک فایل صورت پروژه</a>
+                    </p>
+                </object>
+            )}
         </Layout>
     ) : (
         <Layout title={" آرشیو پروژه‌ها"} description="آرشیو صورت مسئله و کد پاسخ پروژه‌های درس">
