2032b6762ef995cf355bdfc0d684511ec8ac553c
Shahriar-0 <attarshahriar@gmail.com>
2024-09-13 23:37:53 +0330

    add f02 projects archive

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -36,35 +36,79 @@ export default [
         ],
     },
     {
-        section: "ترم پاییز 1403",
+        section: "ترم پاییز 1402",
         list: [
+            {
+                icon: "🧮",
+                number: 0,
+                title: "دست‌گرمی",
+                pdf: "https://raw.githubusercontent.com/AP-ECE-UT/Archive-F02/main/A0%20-%20warm%20up/description.pdf",
+                code: "https://github.com/AP-ECE-UT/Archive-F02/tree/main/A0%20-%20warm%20up",
+                description:
+                "تمرین دست گرمی اول برای آشنایی با ++C و مرور مفاهیم ریاضی" 
+            },
             {
                 icon: "🚈",
                 number: 1,
                 title: "خطوط مترو",
-                pdf: "https://arxiv.org/pdf/1706.03762",
-                code: "https://arxiv.org/pdf/1706.03762",
+                pdf: "https://raw.githubusercontent.com/AP-ECE-UT/Archive-F02/main/A1%20-%20subway/description.pdf",
+                code: "https://github.com/AP-ECE-UT/Archive-F02/tree/main/A1%20-%20subway",
                 description:
-                    "یک دسکریپشن بلند و بالا برای اینکه ببینیم چه میشود آیا این مقدار کافی است یا خیر هان هان هان" +
-                    "یک دسکریپشن بلند و بالا برای اینکه ببینیم چه میشود آیا این مقدار کافی است یا خیر هان هان هان",
+                "در این پروژه شما سعی خواهید کرد با کمک vector ها و string ها یک مسئله روزمره را حل کنید"
             },
             {
                 icon: "🔄",
                 number: 2,
                 title: "بازگشتی و پس‌گرد",
-                pdf: "https://github.com/PashaBarahimi/CPS-Course-Projects/blob/main/CA2-Motion-Based-Authentication%2Fdescription.pdf",
-                code: "https://arxiv.org/pdf/1706.03762",
+                pdf: "https://raw.githubusercontent.com/AP-ECE-UT/Archive-F02/main/A2%20-%20recursive/description.pdf",
+                code: "https://github.com/AP-ECE-UT/Archive-F02/tree/main/A2%20-%20recursive",
                 description:
-                    "یک دسکریپشن بلند و بالا برای اینکه ببینیم چه میشود آیا این مقدار کافی است یا خیر هان هان هان",
+                "در این پروژه شما چند سوال بازگشتی حل خواهید کرد و با این موضوع بیشتر آشنا خواهید شد"
+            },
+            {
+                icon: "🚑",
+                number: 3,
+                title: "مدیریت بیمارستان",
+                pdf: "https://raw.githubusercontent.com/AP-ECE-UT/Archive-F02/main/A3%20-%20hospital%20%20manager/description.pdf",
+                code: "https://github.com/AP-ECE-UT/Archive-F02/tree/main/A3%20-%20hospital%20%20manager",
+                description:
+                "در این پروژه از شما خواسته شده تا یک برنامه برای مدیریت بیمارستان ایجاد کنید"
+            },
+            {
+                icon: "🏦",
+                number: 4,
+                title: "مدیریت بیمارستان",
+                pdf: "https://raw.githubusercontent.com/AP-ECE-UT/Archive-F02/main/A4%20-%20bank%20manager/description.pdf",
+                code: "https://github.com/AP-ECE-UT/Archive-F02/tree/main/A4%20-%20bank%20manager",
+                description:
+                "در این پروژه شما یک سیستم بانکداری الکترونیکی طراحی خواهید کرد"
             },
             {
                 icon: "🎮",
-                number: 3,
-                title: "بازی مین یاب",
-                pdf: "https://docs.google.com/viewer?url=https://raw.githubusercontent.com/PashaBarahimi/CPS-Course-Projects/main/CA2-Motion-Based-Authentication/description.pdf",
-                code: "https://arxiv.org/pdf/1706.03762",
+                number: 5,
+                title: "بازی بمب گذاری",
+                pdf: "https://raw.githubusercontent.com/AP-ECE-UT/Archive-F02/main/A5%20-%20game%20(playing%20with%20fire%202)/description.pdf",
+                code: "https://github.com/AP-ECE-UT/Archive-F02/tree/main/A5%20-%20game%20(playing%20with%20fire%202)",
                 description:
-                    "یک دسکریپشن بلند و بالا برای اینکه ببینیم چه میشود آیا این مقدار کافی است یا خیر هان هان هان",
+                "طراحی یک بازی ساده دو بعدی در فضای ++C و با کمک کتابخانه SFML"
+            },
+            {
+                icon: "🩻",
+                number: 6,
+                title: "فیلترگذاری روی عکس",
+                pdf: "https://raw.githubusercontent.com/AP-ECE-UT/Archive-F02/main/A6%20-%20image%20filtering/description.pdf",
+                code: "https://github.com/AP-ECE-UT/Archive-F02/tree/main/A6%20-%20image%20filtering",
+                description:
+                "طراحی یک برنامه برای فیلترگذاری رو عکس با قابلیت‌های متنوع"
+            },
+            {
+                icon: "🩻",
+                number: 7,
+                title: "فیلترگذاری روی عکس",
+                pdf: "https://raw.githubusercontent.com/AP-ECE-UT/Archive-F02/main/A7%20-%20web%20(sputify)/description.pdf",
+                code: "https://github.com/AP-ECE-UT/Archive-F02/tree/main/A7%20-%20web%20(sputify)",
+                description:
+                "در این پروژه شما برنامه معروف spotify را شبیه‌سازی خواهید کرد و آن را در بستر وب بالا خواهید آورد"
             },
         ],
     },
