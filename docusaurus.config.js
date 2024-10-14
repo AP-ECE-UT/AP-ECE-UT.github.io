3ef6b323f6720c0e59ca32e4db7f6e2903c1e426
MisaghM <misagh.mhgg@gmail.com>
2024-10-14 19:41:28 +0330

    Clean docusaurus config and remove KaTeX stylesheet

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,9 +1,3 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
@@ -12,24 +6,16 @@ const config = {
     tagline: "سایت درس برنامه‌سازی پیشرفته دانشگاه تهران!",
     favicon: "img/favicon.ico",
 
-    // Set the production url of your site here
     url: "https://AP-ECE-UT.github.io",
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: "/",
     trailingSlash: false,
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: "AP-ECE-UT", // Usually your GitHub org/user name.
-    projectName: "AP-ECE-UT.github.io", // Usually your repo name.
+    organizationName: "AP-ECE-UT",
+    projectName: "AP-ECE-UT.github.io",
 
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
 
-    // Even if you don't use internationalization, you can use this field to set
-    // useful metadata like html lang. For example, if your site is Chinese, you
-    // may want to replace "en" with "zh-Hans".
     i18n: {
         defaultLocale: "fa",
         locales: ["fa"],
@@ -47,22 +33,18 @@ const config = {
             {
                 docs: {
                     sidebarPath: "./sidebars.js",
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
                     editUrl: "https://github.com/AP-ECE-UT/AP-ECE-UT.github.io/blob/main/",
                 },
                 blog: {
                     showReadingTime: false,
                     blogTitle: 'مجله',
-                    blogDescription: 'AP Blog !!',
+                    blogDescription: 'AP Blog!',
                     postsPerPage: 'ALL',
-                    blogSidebarTitle: 'جلد های منتشر شده',
+                    blogSidebarTitle: 'جلدهای منتشر شده',
                     blogSidebarCount: 'ALL',
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
                 },
                 theme: {
-                    customCss: "./src/css/custom.css",
+                    customCss: "./src/css/global.css",
                 },
             },
         ],
@@ -71,28 +53,31 @@ const config = {
     themeConfig:
         /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
-            // Replace with your project's social card
             image: "img/ap-social-card.jpg",
             navbar: {
                 title: "برنامه‌سازی پیشرفته",
                 logo: {
-                    alt: "My Site Logo",
+                    alt: "AP Site Logo",
                     src: "img/duck.svg",
                 },
                 items: [
                     {
                         type: "docSidebar",
                         sidebarId: "courseSidebar",
-                        position: "left",
                         label: "محتوای درس",
+                        position: "left",
                     },
                     {
                         type: "docSidebar",
                         sidebarId: "tutorialsSidebar",
-                        position: "left",
                         label: "آموزش‌ها",
+                        position: "left",
                     },
-                    { to: "/blog", label: "مجله", position: "left" },
+                    {
+                        to: "/blog",
+                        label: "مجله",
+                        position: "left",
+                    },
                     {
                         to: "/projects",
                         label: "پروژه‌ها",
@@ -108,17 +93,9 @@ const config = {
             prism: {
                 theme: prismThemes.github,
                 darkTheme: prismThemes.dracula,
-                additionalLanguages: ["powershell", "makefile", "bash"],
+                additionalLanguages: ["makefile", "bash"],
             },
         }),
-    stylesheets: [
-        {
-            href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
-            type: "text/css",
-            integrity: "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
-            crossorigin: "anonymous",
-        }
-    ],
 };
 
 export default config;
