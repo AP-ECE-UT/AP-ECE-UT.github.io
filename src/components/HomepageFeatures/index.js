0a26ddd005c043bcc51699bc26c63f3fdb84e12a
MisaghM <misagh.mhgg@gmail.com>
2024-10-15 01:08:29 +0330

    Refactor homepage features and add link to pages

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,34 +1,38 @@
-import clsx from "clsx";
 import Heading from "@theme/Heading";
+import Link from "@docusaurus/Link";
+
 import styles from "./styles.module.css";
 
-const FeatureList = [
+const featureList = [
     {
         title: "آموزش‌ها",
-        imageSrc: "img/duck-laptop.png",
+        imageSrc: "/img/duck-laptop.png",
         description:
-            "چندین آموزش جامع تهیه شده توسط دستیاران آموزشی درس، درباره کار با لینوکس، تمیزی کد، گیت و گیتهاب و...",
+            "چندین آموزش جامع تهیه شده توسط دستیاران آموزشی درس درباره کار با لینوکس، تمیزی کد، گیت و گیت‌هاب و...",
+        link: "/docs/course/intro",
     },
     {
         title: "ویدیوهای درس",
-        imageSrc: "img/duck-video.png",
+        imageSrc: "/img/duck-video.png",
         description: "آرشیو کامل ویدیوهای درس که توسط استاد ضبط شده‌ است به همراه کدهای مربوط به هر بخش",
+        link: "/docs/tutorials/intro",
     },
     {
         title: "مجله",
-        imageSrc: "img/duck-magazine.png",
+        imageSrc: "/img/duck-magazine.png",
         description: "مجلۀ خواندنی «سی به علاوه یک» که شامل اطلاعات فراتر از درس بوده و به مرور تالیف و منتشر می‌شود",
+        link: "/blog",
     },
 ];
 
-function Feature({ imageSrc, title, description }) {
+function Feature({ title, imageSrc, description, link }) {
     return (
-        <div className={clsx("col col--4")}>
-            <div className="text--center">
-                <img className={styles.featureSvg} src={imageSrc} />
-            </div>
-            <div className="text--center padding-horiz--md">
-                <Heading as="h3">{title}</Heading>
+        <div className="col col--4 text--center">
+            <div className="padding-horiz--md">
+                <Link to={link} className={styles.featureLink}>
+                    <img className={styles.featureSvg} src={imageSrc} alt="Duck" />
+                    <Heading as="h3">{title}</Heading>
+                </Link>
                 <p>{description}</p>
             </div>
         </div>
@@ -37,10 +41,10 @@ function Feature({ imageSrc, title, description }) {
 
 export default function HomepageFeatures() {
     return (
-        <section className={styles.features}>
+        <section className="padding-vert--lg">
             <div className="container">
                 <div className="row">
-                    {FeatureList.map((props, idx) => (
+                    {featureList.map((props, idx) => (
                         <Feature key={idx} {...props} />
                     ))}
                 </div>
