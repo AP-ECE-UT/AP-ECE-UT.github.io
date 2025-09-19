dd368465828404ba40fc8138a1b3fa5bd97dd027
Eliout <elahekhodavrdi@gmail.com>
2025-09-19 13:12:51 +0330

    move former TAs data to another file

--- src/data/assistantsData.js
+++ src/data/assistantsData.js
@@ -1,3 +1,4 @@
+import formerAssistantsData from "./formerAssistantsData";
 export default [
     {
         section: "دستیار ارشد",
@@ -40,7 +41,8 @@ export default [
                 imageURL: "/img/TAs/AmirhosseinArefzadeh.jpg",
                 socials: {
                     github: "https://github.com/Amir-rfz",
-                    linkedin: "https://www.linkedin.com/in/amirhossein-arefzadeh-8612812ba/",
+                    linkedin:
+                        "https://www.linkedin.com/in/amirhossein-arefzadeh-8612812ba/",
                 },
                 presence: {
                     assistant: ["F03", "S04"],
@@ -129,7 +131,8 @@ export default [
                 imageURL: "/img/TAs/ParisaYahyapour.jpg",
                 socials: {
                     github: "https://github.com/parisa-yahyapour",
-                    linkedin: "https://www.linkedin.com/in/parisa-yahyapour-fatideh-47418b284",
+                    linkedin:
+                        "https://www.linkedin.com/in/parisa-yahyapour-fatideh-47418b284",
                 },
                 presence: {
                     assistant: ["S03", "F03", "S04"],
@@ -155,7 +158,7 @@ export default [
                 imageURL: "/img/TAs/RezaChehreghani.jpg",
                 socials: {
                     github: "https://github.com/reza-chehreghani",
-                    linkedin: "https://www.linkedin.com/in/reza-chehreghani-09455b269"
+                    linkedin: "https://www.linkedin.com/in/reza-chehreghani-09455b269",
                 },
                 presence: {
                     assistant: ["F04"],
@@ -219,7 +222,8 @@ export default [
                 imageURL: "/img/TAs/HamidMahmoudi.jpg",
                 socials: {
                     github: "https://github.com/hmdmhmd2004",
-                    linkedin: "https://www.linkedin.com/in/seyed-hamid-mahmoudi-2248512b1",
+                    linkedin:
+                        "https://www.linkedin.com/in/seyed-hamid-mahmoudi-2248512b1",
                 },
                 presence: {
                     assistant: ["S04"],
@@ -232,7 +236,8 @@ export default [
                 imageURL: "/img/TAs/SadeghSamadi.jpg",
                 socials: {
                     github: "https://github.com/imSaDy",
-                    linkedin: "https://https://www.linkedin.com/in/sadegh-samadi-3643b11b7",
+                    linkedin:
+                        "https://https://www.linkedin.com/in/sadegh-samadi-3643b11b7",
                 },
                 presence: {
                     assistant: ["S04"],
@@ -389,197 +394,5 @@ export default [
             },
         ],
     },
-    {
-        section: "دستیاران آموزشی پیشین",
-        list: [
-            {
-                name: "آرین باستانی",
-                title: "ورودی 1400",
-                email: "bastanyaryn@gmail.com",
-                imageURL: "/img/TAs/AryanBastani.jpg",
-                socials: {
-                    github: "https://github.com/AryanBastani",
-                },
-                presence: {
-                    assistant: ["F03"],
-                },
-            },
-            {
-                name: "امیرعلی وحیدی",
-                title: "ورودی 1399",
-                email: "amirali.vahidi01@gmail.com",
-                imageURL: "/img/TAs/AmiraliVahidi.jpg",
-                socials: {
-                    github: "https://github.com/Mohta3b",
-                    linkedin: "https://www.linkedin.com/in/amirali-vahidi-b53094227/",
-                },
-                presence: {
-                    assistant: ["S03", "F03"],
-                },
-            },
-            {
-                name: "آوا میرمحمدمهدی",
-                title: "ورودی 1399",
-                email: "avamir80@gmail.com",
-                imageURL: "/img/TAs/AvaMirmohammadmahdi.jpg",
-                socials: {
-                    github: "https://github.com/avamirm",
-                },
-                presence: {
-                    assistant: ["S03", "F03"],
-                },
-            },
-            {
-                name: "پاشا براهیمی",
-                title: "ورودی 1399",
-                email: "pashabarahimi@gmail.com",
-                imageURL: "/img/TAs/PashaBarahimi.jpg",
-                socials: {
-                    github: "https://github.com/PashaBarahimi",
-                    linkedin: "https://www.linkedin.com/in/PashaBarahimi",
-                },
-                presence: {
-                    chief: ["S03"],
-                },
-            },
-            {
-                name: "سامان اسلامی نظری",
-                title: "ورودی 1399",
-                email: "samaneslami78@gmail.com",
-                imageURL: "/img/TAs/SamanEslami.jpg",
-                socials: {
-                    github: "https://github.com/SamanEn",
-                    linkedin: "https://www.linkedin.com/in/saman-eslami-nazari",
-                },
-                presence: {
-                    chief: ["S03"],
-                },
-            },
-            {
-                name: "عرفان عسگری",
-                title: "ورودی 1399",
-                email: "erfanasgari21@gmail.com",
-                imageURL: "/img/TAs/ErfanAsgari.jpg",
-                socials: {
-                    github: "https://github.com/erfanasgari21",
-                    linkedin: "https://www.linkedin.com/in/erfan-asgari-b748701b4/",
-                },
-                presence: {
-                    assistant: ["S03", "F03"],
-                },
-            },
-            {
-                name: "عرفان میرشمس",
-                title: "ورودی 1401",
-                email: "erfan.mirshams@gmail.com",
-                imageURL: "/img/TAs/ErfanMirshams.jpg",
-                socials: {
-                    github: "https://github.com/erfan-mirshams",
-                    linkedin: "https://www.linkedin.com/in/erfan-mirshams",
-                },
-                presence: {
-                    assistant: ["S03", "F03"],
-                },
-            },
-            {
-                name: "علی پادیاو",
-                title: "ورودی 1399",
-                email: "apadyav@gmail.com",
-                imageURL: "/img/TAs/AliPadyav.jpg",
-                socials: {
-                    github: "https://github.com/alumpish",
-                },
-                presence: {
-                    assistant: ["S03", "F03"],
-                },
-            },
-            {
-                name: "علی حمزه‌پور",
-                title: "ورودی 1400",
-                email: "Alihamzehpour2002@gmail.com",
-                imageURL: "/img/TAs/AliHamzepour.jpg",
-                socials: {
-                    github: "https://github.com/AliHamze2002",
-                },
-                presence: {
-                    assistant: ["S03"],
-                },
-            },
-            {
-                name: "علی عطااللهی",
-                title: "ورودی 1399",
-                email: "aliataollahi40@gmail.com",
-                imageURL: "/img/TAs/AliAtaollahi.jpg",
-                socials: {
-                    github: "https://github.com/AliAtaollahi",
-                    linkedin: "https://www.linkedin.com/in/ali-ataollahi-b46302281",
-                },
-                presence: {
-                    assistant: ["S03"],
-                },
-            },
-            {
-                name: "مبینا مهرآذر",
-                title: "ورودی 1400",
-                email: "mobinamehrazar81@gmail.com",
-                imageURL: "/img/TAs/MobinaMehrazar.jpg",
-                socials: {
-                    github: "https://github.com/MobinaMhr",
-                },
-                presence: {
-                    assistant: ["S03", "F03"],
-                },
-            },
-            {
-                name: "مهدی نوری",
-                title: "ورودی 1400",
-                email: "hitking2007@gmail.com",
-                imageURL: "/img/TAs/MahdiNoori.jpg",
-                socials: {
-                    github: "https://github.com/MahdiNoori2003",
-                },
-                presence: {
-                    assistant: ["S03"],
-                },
-            },
-            {
-                name: "میثاق محقق",
-                title: "ورودی 1399",
-                email: "misagh.mhgg@gmail.com",
-                imageURL: "/img/TAs/MisaghMohaghegh.jpg",
-                socials: {
-                    github: "https://github.com/MisaghM",
-                    linkedin: "https://www.linkedin.com/in/misaghm",
-                },
-                presence: {
-                    assistant: ["S03", "F03"],
-                },
-            },
-            {
-                name: "نسا عباسی",
-                title: "ورودی 1399",
-                email: "nesaabbasi4@gmail.com",
-                imageURL: "/img/TAs/NesaAbbasi.jpg",
-                socials: {
-                    github: "https://github.com/Nesabbasi",
-                },
-                presence: {
-                    assistant: ["S03", "F03"],
-                },
-            },
-            {
-                name: "هادی بابالو",
-                title: "ورودی 1399",
-                email: "m.h.babalu@gmail.com",
-                imageURL: "/img/TAs/HadiBabalou.jpg",
-                socials: {
-                    github: "https://github.com/Hadi-loo",
-                    linkedin: "https://www.linkedin.com/in/hadi-babaloo/",
-                },
-                presence: {
-                    assistant: ["S03", "F03"],
-                },
-            },
-        ],
-    },
+    formerAssistantsData,
 ];
---
+++ src/data/formerAssistantsData.js
@@ -0,0 +1,195 @@
+const formerAssistantsData = {
+    section: "دستیاران آموزشی پیشین",
+    list: [
+        {
+            name: "آرین باستانی",
+            title: "ورودی 1400",
+            email: "bastanyaryn@gmail.com",
+            imageURL: "/img/TAs/AryanBastani.jpg",
+            socials: {
+                github: "https://github.com/AryanBastani",
+            },
+            presence: {
+                assistant: ["F03"],
+            },
+        },
+        {
+            name: "امیرعلی وحیدی",
+            title: "ورودی 1399",
+            email: "amirali.vahidi01@gmail.com",
+            imageURL: "/img/TAs/AmiraliVahidi.jpg",
+            socials: {
+                github: "https://github.com/Mohta3b",
+                linkedin: "https://www.linkedin.com/in/amirali-vahidi-b53094227/",
+            },
+            presence: {
+                assistant: ["S03", "F03"],
+            },
+        },
+        {
+            name: "آوا میرمحمدمهدی",
+            title: "ورودی 1399",
+            email: "avamir80@gmail.com",
+            imageURL: "/img/TAs/AvaMirmohammadmahdi.jpg",
+            socials: {
+                github: "https://github.com/avamirm",
+            },
+            presence: {
+                assistant: ["S03", "F03"],
+            },
+        },
+        {
+            name: "پاشا براهیمی",
+            title: "ورودی 1399",
+            email: "pashabarahimi@gmail.com",
+            imageURL: "/img/TAs/PashaBarahimi.jpg",
+            socials: {
+                github: "https://github.com/PashaBarahimi",
+                linkedin: "https://www.linkedin.com/in/PashaBarahimi",
+            },
+            presence: {
+                chief: ["S03"],
+            },
+        },
+        {
+            name: "سامان اسلامی نظری",
+            title: "ورودی 1399",
+            email: "samaneslami78@gmail.com",
+            imageURL: "/img/TAs/SamanEslami.jpg",
+            socials: {
+                github: "https://github.com/SamanEn",
+                linkedin: "https://www.linkedin.com/in/saman-eslami-nazari",
+            },
+            presence: {
+                chief: ["S03"],
+            },
+        },
+        {
+            name: "عرفان عسگری",
+            title: "ورودی 1399",
+            email: "erfanasgari21@gmail.com",
+            imageURL: "/img/TAs/ErfanAsgari.jpg",
+            socials: {
+                github: "https://github.com/erfanasgari21",
+                linkedin: "https://www.linkedin.com/in/erfan-asgari-b748701b4/",
+            },
+            presence: {
+                assistant: ["S03", "F03"],
+            },
+        },
+        {
+            name: "عرفان میرشمس",
+            title: "ورودی 1401",
+            email: "erfan.mirshams@gmail.com",
+            imageURL: "/img/TAs/ErfanMirshams.jpg",
+            socials: {
+                github: "https://github.com/erfan-mirshams",
+                linkedin: "https://www.linkedin.com/in/erfan-mirshams",
+            },
+            presence: {
+                assistant: ["S03", "F03"],
+            },
+        },
+        {
+            name: "علی پادیاو",
+            title: "ورودی 1399",
+            email: "apadyav@gmail.com",
+            imageURL: "/img/TAs/AliPadyav.jpg",
+            socials: {
+                github: "https://github.com/alumpish",
+            },
+            presence: {
+                assistant: ["S03", "F03"],
+            },
+        },
+        {
+            name: "علی حمزه‌پور",
+            title: "ورودی 1400",
+            email: "Alihamzehpour2002@gmail.com",
+            imageURL: "/img/TAs/AliHamzepour.jpg",
+            socials: {
+                github: "https://github.com/AliHamze2002",
+            },
+            presence: {
+                assistant: ["S03"],
+            },
+        },
+        {
+            name: "علی عطااللهی",
+            title: "ورودی 1399",
+            email: "aliataollahi40@gmail.com",
+            imageURL: "/img/TAs/AliAtaollahi.jpg",
+            socials: {
+                github: "https://github.com/AliAtaollahi",
+                linkedin: "https://www.linkedin.com/in/ali-ataollahi-b46302281",
+            },
+            presence: {
+                assistant: ["S03"],
+            },
+        },
+        {
+            name: "مبینا مهرآذر",
+            title: "ورودی 1400",
+            email: "mobinamehrazar81@gmail.com",
+            imageURL: "/img/TAs/MobinaMehrazar.jpg",
+            socials: {
+                github: "https://github.com/MobinaMhr",
+            },
+            presence: {
+                assistant: ["S03", "F03"],
+            },
+        },
+        {
+            name: "مهدی نوری",
+            title: "ورودی 1400",
+            email: "hitking2007@gmail.com",
+            imageURL: "/img/TAs/MahdiNoori.jpg",
+            socials: {
+                github: "https://github.com/MahdiNoori2003",
+            },
+            presence: {
+                assistant: ["S03"],
+            },
+        },
+        {
+            name: "میثاق محقق",
+            title: "ورودی 1399",
+            email: "misagh.mhgg@gmail.com",
+            imageURL: "/img/TAs/MisaghMohaghegh.jpg",
+            socials: {
+                github: "https://github.com/MisaghM",
+                linkedin: "https://www.linkedin.com/in/misaghm",
+            },
+            presence: {
+                assistant: ["S03", "F03"],
+            },
+        },
+        {
+            name: "نسا عباسی",
+            title: "ورودی 1399",
+            email: "nesaabbasi4@gmail.com",
+            imageURL: "/img/TAs/NesaAbbasi.jpg",
+            socials: {
+                github: "https://github.com/Nesabbasi",
+            },
+            presence: {
+                assistant: ["S03", "F03"],
+            },
+        },
+        {
+            name: "هادی بابالو",
+            title: "ورودی 1399",
+            email: "m.h.babalu@gmail.com",
+            imageURL: "/img/TAs/HadiBabalou.jpg",
+            socials: {
+                github: "https://github.com/Hadi-loo",
+                linkedin: "https://www.linkedin.com/in/hadi-babaloo/",
+            },
+            presence: {
+                assistant: ["S03", "F03"],
+            },
+        },
+    ],
+};
+
+export default formerAssistantsData;
