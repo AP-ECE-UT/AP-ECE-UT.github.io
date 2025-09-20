03bde5e348af57213109607fac2bd59d16214416
Eliout <elahekhodavrdi@gmail.com>
2025-09-20 22:19:43 +0330

    add first part of course videos data

---
+++ src/data/courseData.js
@@ -0,0 +1,97 @@
+const courseVideos = {
+  1: {
+    label: "1 - مقدمه‌ای بر ++C",
+    subSections: {
+      1: {
+        label: "ورودی و خروجی پایه در ++C",
+        videoURL: "https://drive.google.com/file/d/1cizfSIYiyYDG5b2w9rkEhWu6gsrhBgNY",
+        githubURL: "https://github.com/AP-ECE-UT/AP-Notes/blob/archive/01_Basics/01_HelloWorld.cpp",
+      },
+      2: {
+        label: "ورودی و خروجی در محیط ترمینال",
+        videoURL: "https://drive.google.com/file/d/1D4KjSCztUzyV35kPmvkq49U9c-8gbuko",
+        githubURL: "https://github.com/AP-ECE-UT/AP-Notes/blob/archive/01_Basics/04_DeleteRepeated.cpp",
+      },
+      3: {
+        label: "کد خروج از برنامه",
+        videoURL: "https://drive.google.com/file/d/150sUtBikxGYdq4R_J8VxP1wcJmNaIVPI",
+        githubURL: "https://github.com/AP-ECE-UT/AP-Notes/blob/archive/01_Basics/ExitTest.cpp",
+      },
+      4: {
+        label: "کار با بردارها",
+        videoURL: "https://drive.google.com/file/d/18XT5zRu9UPQI8oXgJYki_RZ_bZVJ7QmL",
+        githubURL: "https://github.com/AP-ECE-UT/AP-Notes/blob/archive/02_Vectors/01_MeanMedian.cpp",
+      },
+    }
+  },
+  2: {
+    label: "",
+    subSections: {}
+  },
+  3: {
+    label: "",
+    subSections: {}
+  },
+  4: {
+    label: "",
+    subSections: {}
+  },
+  5: {
+    label: "",
+    subSections: {}
+  },
+  6: {
+    label: "",
+    subSections: {}
+  },
+  7: {
+    label: "",
+    subSections: {}
+  },
+  8: {
+    label: "",
+    subSections: {}
+  },
+  9: {
+    label: "",
+    subSections: {}
+  },
+  10: {
+    label: "",
+    subSections: {}
+  },
+  11: {
+    label: "",
+    subSections: {}
+  },
+  12: {
+    label: "",
+    subSections: {}
+  },
+  13: {
+    label: "",
+    subSections: {}
+  },
+  14: {
+    label: "",
+    subSections: {}
+  },
+  15: {
+    label: "",
+    subSections: {}
+  },
+  16: {
+    label: "",
+    subSections: {}
+  },
+  17: {
+    label: "",
+    subSections: {}
+  },
+  18: {
+    label: "",
+    subSections: {}
+  }
+};
+
+export default courseVideos;
